9bce81d6d90d6ca197c384f54ca5c428a6a0748d
Nitika Goel <ngoel9@sapient.com>
2015-09-11 09:46:24 +0530

    Sonar Violation demo checkin

--- src/ComplexMethod.java
+++ src/ComplexMethod.java
@@ -19,7 +19,10 @@ public class ComplexMethod {
         if (i == 6) {
             return "Six";
         }
-        if (i > 6) {
+		 if (i == 7) {
+            return "seven";
+        }
+        if (i > 8) {
             throw new NotImplementedException();
         }
         return null;
